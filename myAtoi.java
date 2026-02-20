e2796ca6b635364d8b0596f1f4ecd1d9d007f464
Hansika_Agrawal <hansikaagrawal930@gmail.com>
2026-02-20 13:08:21 +0530

    Implement myAtoi function for string to integer conversion

---
+++ myAtoi.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0;
+        int n = s.length();
+        int sign = 1;
+        int num = 0;
+
+        // 1. Skip leading spaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // 2. Check sign
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i) == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+
+        // 3. Convert digits
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            // 4. Check overflow BEFORE it happens
+            if (num > Integer.MAX_VALUE / 10 ||
+               (num == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            num = num * 10 + digit;
+            i++;
+        }
+
+        return num * sign;
+    }
+}
